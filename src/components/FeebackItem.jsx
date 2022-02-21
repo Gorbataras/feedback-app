77417f3470becd18df7bfa2631ef10853d1cd277
Gorbataras <47342116+Gorbataras@users.noreply.github.com>
2022-02-21 01:05:39 -0800

    Building feedback item component

---
+++ src/components/FeebackItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FeebackItem() {
+  return (
+        <div className="card">
+            <div className="num-display">10</div>
+            <div className="text-display">text disp</div>
+        </div>
+  )
+}
+
+export default FeebackItem
