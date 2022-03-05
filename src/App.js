ae370ae528ce5c1a97f652a277be2c63fe3b48e1
Gorbataras <47342116+Gorbataras@users.noreply.github.com>
2022-03-05 03:58:43 -0800

    Created Feedback Context and provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {v4 as uuidv4} from 'uuid'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {FeedbackProvider} from './context/FeedbackContext'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
@@ -8,6 +9,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
+
 
 function App() {
 
@@ -25,6 +27,7 @@ function App() {
     }
 
     return (
+        <FeedbackProvider>
         <Router>
             <Header />
             <div className="container">
@@ -40,10 +43,12 @@ function App() {
                     }/>
                     
                     <Route path='/about' element={<AboutPage/>} />
+
                 </Routes>
                 <AboutIconLink />
             </div>
         </Router>   
+        </FeedbackProvider>
     )
 }
 
